134e24fd5410f9b3087b9631d6a5b89a53b841a7
aibek2050 <aibek.yerzhanuly@gmail.com>
2023-05-14 00:07:18 +0600

    added top header

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,19 +1,23 @@
 <template>
-  <div id="top-header">
-    <div>
-      <div>
+  <div class="bg-default text-white py-3">
+    <div class="container d-flex justify-content-between align-items-center">
+      <div class="pointer">
         <span>+7 727 349 97 40</span>
       </div>
-      <div>
-        <div>
+      <div class="d-flex">
+        <div class="pointer">
           <font-awesome-icon :icon="['fas', 'globe']" />
-          <span>English</span>
+          <span class="ms-1">English</span>
         </div>
-        <div>
+        <div class="ms-4 pointer">
           <font-awesome-icon :icon="['fas', 'user']" />
-          <span>My order</span>
+          <span class="ms-1">My order</span>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import "../assets/css/styles.css";
+</script>
