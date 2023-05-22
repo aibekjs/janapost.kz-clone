6920d32f78fd91240ba2e9ba87f0b998bb6fbdad
aibek2050 <aibek.yerzhanuly@gmail.com>
2023-05-22 14:24:16 +0600

    added hero text

---
+++ src/components/HeroText.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <span class="d-block hero-title fw-bold"
+      >Қытайдан онлайн сатып алыңыз, Қазақстанға жеткіземіз!</span
+    >
+    <span class="d-block">janapost.kz - тауарларды жедел жеткізу</span>
+  </div>
+</template>
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <primary-button />
+    <hero-text />
   </div>
 </template>
 
 <script setup lang="ts">
 import PrimaryButton from "@/components/PrimaryButton.vue";
+import HeroText from "@/components/HeroText.vue";
 </script>
