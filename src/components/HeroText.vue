e1e773167902cdb4726489521b5a0ec67edec256
aibek2050 <aibek.yerzhanuly@gmail.com>
2023-05-22 15:07:38 +0600

    added hero

--- src/components/HeroText.vue
+++ src/components/HeroText.vue
@@ -3,6 +3,6 @@
     <span class="d-block hero-title fw-bold"
       >Қытайдан онлайн сатып алыңыз, Қазақстанға жеткіземіз!</span
     >
-    <span class="d-block">janapost.kz - тауарларды жедел жеткізу</span>
+    <span class="d-block pt-2">janapost.kz - тауарларды жедел жеткізу</span>
   </div>
 </template>
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <primary-button />
-    <hero-text />
+  <div class="container-fluid pt-5 bg-primary">
+    <div class="row">
+      <div class="col-6" style="padding: 100px 0 0 100px">
+        <hero-text />
+        <primary-button class="pt-5" />
+      </div>
+      <div class="col-6">
+        <img
+          src="@/assets/images/hero-img1.png"
+          alt="img"
+          style="width: 100%"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
