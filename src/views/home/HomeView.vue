a8dae663fab76ac85ff9db195f17540ead76235e
aibek2050 <aibek.yerzhanuly@gmail.com>
2023-05-22 18:45:13 +0600

    added carousel

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,16 +1,55 @@
 <template>
-  <div class="container-fluid pt-5 bg-primary">
-    <div class="row">
-      <div class="col-6" style="padding: 100px 0 0 100px">
-        <hero-text />
-        <primary-button class="pt-5" />
+  <div
+    id="carouselExampleIndicators"
+    class="carousel slide"
+    data-bs-ride="carousel"
+  >
+    <div class="carousel-indicators">
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="0"
+        class="active"
+        aria-current="true"
+        aria-label="Slide 1"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="1"
+        aria-label="Slide 2"
+      ></button>
+    </div>
+    <div class="carousel-inner pt-5">
+      <div class="carousel-item active bg-primary" data-bs-interval="3000">
+        <div class="row">
+          <div class="col-6" style="padding: 100px 0 0 100px">
+            <hero-text />
+            <primary-button class="pt-5" />
+          </div>
+          <div class="col-6">
+            <img
+              src="@/assets/images/hero-img1.png"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
       </div>
-      <div class="col-6">
-        <img
-          src="@/assets/images/hero-img1.png"
-          alt="img"
-          style="width: 100%"
-        />
+      <div class="carousel-item bg-danger" data-bs-interval="3000">
+        <div class="row">
+          <div class="col-6" style="padding: 100px 0 0 100px">
+            <hero-text />
+            <primary-button class="pt-5" />
+          </div>
+          <div class="col-6">
+            <img
+              src="@/assets/images/hero-img1.png"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
